38ced0b480b31a10848ca2288a54f8d3292af36a
tripalltime2026 <tookokuchukhidze@gmail.com>
2026-07-29 17:46:08 +0400

    Use file cache before database provisioning

--- config/cache.php
+++ config/cache.php
@@ -1,10 +1,14 @@
 <?php
 return [
-    'default' => env('CACHE_STORE', 'database'),
+    'default' => env('CACHE_STORE', 'file'),
     'stores' => [
         'array' => ['driver' => 'array', 'serialize' => false],
         'database' => ['driver' => 'database', 'table' => 'cache', 'lock_table' => 'cache_locks'],
-        'file' => ['driver' => 'file', 'path' => storage_path('framework/cache/data'), 'lock_path' => storage_path('framework/cache/data')],
+        'file' => [
+            'driver' => 'file',
+            'path' => storage_path('framework/cache/data'),
+            'lock_path' => storage_path('framework/cache/data'),
+        ],
     ],
     'prefix' => 'inesbaghi_cache_',
 ];
