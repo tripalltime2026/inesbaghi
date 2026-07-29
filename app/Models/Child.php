1456c0657eba5462b3bb6e662b62dde7292ffcf3
tripalltime2026 <tookokuchukhidze@gmail.com>
2026-07-29 15:26:39 +0400

    Add child model

---
+++ app/Models/Child.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Child extends Model
+{
+    protected $fillable = ['first_name', 'last_name', 'birth_date', 'birth_year', 'medical_notes', 'photo_consent_at'];
+
+    protected function casts(): array
+    {
+        return [
+            'birth_date' => 'date',
+            'photo_consent_at' => 'datetime',
+        ];
+    }
+
+    public function guardians(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'child_guardians')
+            ->withPivot(['relationship', 'is_primary', 'can_pick_up'])
+            ->withTimestamps();
+    }
+
+    public function enrollments(): HasMany
+    {
+        return $this->hasMany(Enrollment::class);
+    }
+}
