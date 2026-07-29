445f07fc17f4e715b61a035d1a2c5ac2aa114d19
tripalltime2026 <tookokuchukhidze@gmail.com>
2026-07-29 16:41:23 +0400

    Add configurable demo authentication

--- config/services.php
+++ config/services.php
@@ -5,4 +5,8 @@ return [
         'otp_ttl_minutes' => (int) env('OTP_TTL_MINUTES', 5),
         'otp_max_attempts' => (int) env('OTP_MAX_ATTEMPTS', 5),
     ],
+    'demo_auth' => [
+        'enabled' => (bool) env('DEMO_AUTH_ENABLED', false),
+        'admin_phone' => env('DEMO_ADMIN_PHONE', '555411831'),
+    ],
 ];
