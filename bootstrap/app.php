fb9aa4914aa6eefef8f9c80e707716ac638155a8
tripalltime2026 <tookokuchukhidze@gmail.com>
2026-07-29 21:57:25 +0400

    Register managed content middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ApplyManagedContent;
 use App\Http\Middleware\EnsureUserHasRole;
 use App\Http\Middleware\InjectResponsiveAssets;
 use Illuminate\Foundation\Application;
@@ -9,7 +10,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(web: __DIR__.'/../routes/web.php', commands: __DIR__.'/../routes/console.php', health: '/up')
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [InjectResponsiveAssets::class]);
+        $middleware->web(append: [InjectResponsiveAssets::class, ApplyManagedContent::class]);
         $middleware->alias(['role' => EnsureUserHasRole::class]);
     })
     ->withExceptions(fn (Exceptions $exceptions) => null)
