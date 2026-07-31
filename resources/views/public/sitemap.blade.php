3dccf6bf475cfe09fe62fb00cfacbe34eb381c2c
tripalltime2026 <tookokuchukhidze@gmail.com>
2026-07-31 15:33:28 +0400

    Include article modification dates in sitemap

--- resources/views/public/sitemap.blade.php
+++ resources/views/public/sitemap.blade.php
@@ -3,6 +3,9 @@
 @foreach($urls as $url)
     <url>
         <loc>{{ $url['loc'] }}</loc>
+@if(!empty($url['lastmod']))
+        <lastmod>{{ $url['lastmod'] }}</lastmod>
+@endif
         <priority>{{ $url['priority'] }}</priority>
     </url>
 @endforeach
