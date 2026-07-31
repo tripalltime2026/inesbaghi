a1a5b13c45e7a963d6c9028e232174b255cef1e8
tripalltime2026 <tookokuchukhidze@gmail.com>
2026-07-31 11:49:43 +0400

    Register mobile UX response middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use App\Http\Middleware\ApplyManagedContent;
 use App\Http\Middleware\EnsureParentClubAccess;
 use App\Http\Middleware\EnsureUserHasRole;
+use App\Http\Middleware\InjectMobileUxFixes;
 use App\Http\Middleware\InjectResponsiveAssets;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -11,7 +12,11 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(web: __DIR__.'/../routes/web.php', commands: __DIR__.'/../routes/console.php', health: '/up')
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [InjectResponsiveAssets::class, ApplyManagedContent::class]);
+        $middleware->web(append: [
+            InjectMobileUxFixes::class,
+            InjectResponsiveAssets::class,
+            ApplyManagedContent::class,
+        ]);
         $middleware->alias([
             'role' => EnsureUserHasRole::class,
             'parent.club' => EnsureParentClubAccess::class,
