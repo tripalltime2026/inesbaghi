347ab22d9492e06cfa3b84c9e014a85438aff11d
tripalltime2026 <tookokuchukhidze@gmail.com>
2026-08-03 19:20:35 +0400

    Test simplified registration and visit labels

---
+++ tests/Feature/PublicRegistrationVisitLabelsTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PublicRegistrationVisitLabelsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_registration_uses_simple_heading(): void
+    {
+        $this->get('/registratsia')
+            ->assertOk()
+            ->assertSee('<h1>რეგისტრაცია</h1>', false)
+            ->assertDontSee('რეგისტრაცია ორ ნაბიჯში');
+    }
+
+    public function test_public_pages_use_visit_as_the_action_label(): void
+    {
+        $this->get('/')
+            ->assertOk()
+            ->assertSee('>ვიზიტი</button>', false)
+            ->assertSee('ვიზიტის დაგეგმვა')
+            ->assertDontSee('ჩარიცხვის განაცხადი');
+
+        $this->get('/charetskhva')
+            ->assertOk()
+            ->assertSee('დაგეგმეთ გაცნობითი ვიზიტი')
+            ->assertSee('ვიზიტის მოთხოვნა')
+            ->assertDontSee('>ჩარიცხვა</a>', false);
+    }
+}
